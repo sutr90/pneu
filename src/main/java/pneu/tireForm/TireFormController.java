c9ee47e1c4a2e5e0377907abe02756d2df093ceb
Vojtěch Frič <you@example.com>
2017-11-12 16:01:36 +0100

    Added annotations.

--- src/main/java/pneu/tireForm/TireFormController.java
+++ src/main/java/pneu/tireForm/TireFormController.java
@@ -12,26 +12,47 @@ import javax.inject.Inject;
 public class TireFormController {
     @FXML
     public Button cancelButton;
+    @FXML
     public Button okButton;
+    @FXML
     public TextField size;
+    @FXML
     public TextField radius;
+    @FXML
     public TextField count;
+    @FXML
     public TextField frontRight;
+    @FXML
     public TextField frontLeft;
+    @FXML
     public TextField rearLeft;
+    @FXML
     public TextField rearRight;
+    @FXML
     public TextField price;
+    @FXML
     public SegmentedButton tireSegButton;
+    @FXML
     public SegmentedButton rimSegButton;
+    @FXML
     public Button contractButton;
+    @FXML
     public Button returnButton;
+    @FXML
     public DatePicker dateFrom;
+    @FXML
     public DatePicker dateTo;
+    @FXML
     public TextField name;
+    @FXML
     public TextField surname;
+    @FXML
     public TextField city;
+    @FXML
     public TextField street;
+    @FXML
     public TextField licensePlate;
+
     @Inject
     private StorageService storageService;
 
