b55913394b5333c2435108aeb6b5e1f466926e90
Vojtěch Frič <you@example.com>
2017-11-12 10:56:11 +0100

    Added tire form.

---
+++ src/main/java/pneu/tireForm/TireFormController.java
@@ -0,0 +1,4 @@
+package pneu.tireForm;
+
+public class TireFormController {
+}
---
+++ src/main/java/pneu/tireForm/TireFormView.java
@@ -0,0 +1,6 @@
+package pneu.tireForm;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+public class TireFormView extends FXMLView {
+}
