b23e9a61f6834914795c6a175c1f9b3b41d2bb28
Vojtěch Frič <you@example.com>
2017-11-12 15:45:55 +0100

    Added ids.

--- src/main/java/pneu/tireForm/TireFormController.java
+++ src/main/java/pneu/tireForm/TireFormController.java
@@ -1,10 +1,37 @@
 package pneu.tireForm;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import org.controlsfx.control.SegmentedButton;
 import pneu.storage.StorageService;
 
 import javax.inject.Inject;
 
 public class TireFormController {
+    @FXML
+    public Button cancelButton;
+    public Button okButton;
+    public TextField size;
+    public TextField radius;
+    public TextField count;
+    public TextField frontRight;
+    public TextField frontLeft;
+    public TextField rearLeft;
+    public TextField rearRight;
+    public TextField price;
+    public SegmentedButton tireSegButton;
+    public SegmentedButton rimSegButton;
+    public Button contractButton;
+    public Button returnButton;
+    public DatePicker dateFrom;
+    public DatePicker dateTo;
+    public TextField name;
+    public TextField surname;
+    public TextField city;
+    public TextField street;
+    public TextField licensePlate;
     @Inject
     private StorageService storageService;
 
